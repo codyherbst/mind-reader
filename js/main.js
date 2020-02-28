5c884c776edbd987ff3a9496427e7f70fb997451
Cody Herbst <codyherbst@DESKTOP-QM9SFK1.localdomain>
2020-02-28 16:52:40 -0500

    fix issues from demo day

--- js/main.js
+++ js/main.js
@@ -1,32 +1,37 @@
-//random int function off of w3 school
+//random int function off of w3 school. this takes a max integer and returns it from 0-max
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
 //building the array for random characters with numbers
 var arrFinal = []
+var strFinal = ''
+let ninthVar;
 function numbers() {
     let strCompare = "!@#$%^&*"
+    ninthVar = strCompare.charAt(getRandomInt(strCompare.length))
     for (i = 0; i <= 99; i++) {
-        arrFinal.push(i + " = " + strCompare.charAt(getRandomInt(8)));
+        arrFinal.push(i + " = " + strCompare.charAt(getRandomInt(strCompare.length))); //using above function to get a random character from above string
         if (i % 9 == 0) {
-            arrFinal[i] = i + ' = &';
+            arrFinal[i] = i + ' = ' + ninthVar; //setting every divisible by nine to specific character to make magic work
         }
     }
-    return arrFinal;
-}
-numbers();
 
-//setting string to be new line to display on 5th pane
-let strFinal = ''
-for (i = 0; i <= 98; i++) {
+    strFinal = ''
+    for (i = 0; i <= 98; i++) {
     strFinal += arrFinal[i] + '<br>'
 }
+}
 
-//function for reset button to actually reset mind reader
+//setting string to be new line to display on 5th pane and building the list to display
+
+
+//function for reset button to actually reset mind reader. 
 function resetMR() {
     if (document.getElementById("botRightBtn").onclick && currState > 1) {
         currState = 0;
+
+        //five points below to set up first page on reset. switching state only didn't fix it to original page
         document.getElementById("botRightBtn").innerHTML = "Go!";
         document.getElementById("bigText").innerHTML = "I can read your mind!";
         document.getElementById("mainBtn").style.display = "none";
@@ -35,7 +40,7 @@ function resetMR() {
     }
 }
 
-//function to 
+//function to change state on click
 let currState = 0;
 document.getElementById("mainBtn").style.display = "none";
 function stateChange() {
@@ -44,8 +49,9 @@ function stateChange() {
     if (currState === 6) {
         currState = 0;
     }
-
-    console.log(currState);
+    
+    // console.log(currState);
+    //switch case updates page based on current state. this updates using on click in html
     switch (currState) {
         case 0:
             //default page. needs go button, switches after this state
@@ -62,13 +68,13 @@ function stateChange() {
             document.getElementById("bigText").innerHTML = "Choose a number from 1-99";
             document.getElementById("mainBtn").style.display = "block";
             document.getElementById("subTxt").innerHTML = "When you have your number click next!";
-            // if (document.getElementById("mainBtn").onclick) console.log('it worked')
+            // if (document.getElementById("mainBtn").onclick) console.log('it worked') -- this didn't work as expected -- tried to use this and below one to reset function but it listened for all clicks?
             break;
         case 2:
             //add numbers together
             document.getElementById("bigText").innerHTML = "Add both digits together to get a new number";
             document.getElementById("subTxt").innerHTML = "Ex: 14 is 1 + 4 = 5";
-            // if (document.getElementById("botRightBtn").onclick) console.log('it worked')
+            // if (document.getElementById("botRightBtn").onclick) console.log('it worked') -- this didn't work as expected
             break;
         case 3:
             //subtract new number from original
@@ -77,17 +83,18 @@ function stateChange() {
             break;
         case 4:
             //find new number
+            numbers();
             document.getElementById("bigText").innerHTML = strFinal;
             document.getElementById("subTxt").innerHTML = "Find your new number. Take note of the associated symbol.";
             document.getElementById("mainBtn").innerHTML = "Reveal";
             break;
         case 5:
             //reveal answer
-            document.getElementById("bigText").innerHTML = "&";
+            document.getElementById("bigText").innerHTML = ninthVar;
             document.getElementById("mainBtn").style.display = "none";
-            document.getElementById("subTxt").innerHTML = "Your symbol is &";
+            document.getElementById("subTxt").innerHTML = "Your symbol is " + ninthVar;
             break;
-        default:
+        default: numbers();
     
     
     }
